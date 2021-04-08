6b99fe2c6ccf5b9b280b04f46c88ccd41423b488
unclickifet <unclickifet@gmail.com>
2021-04-08 10:12:38 +0200

    ok

--- server_mine/rcon.py
+++
@@ -1,6 +0,0 @@
-from rcon import Client
-
-with Client('51.68.206.112', 25591) as client:
-    response = client.run('some_command', 'with', 'some', 'arguments')
-
-print(response)
--- server_mine/sincro_mine.py
+++
@@ -1,9 +0,0 @@
-from mcipc.query import Client # Importamos libreria
-from mcipc.rcon.je import Biome, Client
-
-with Client('51.68.206.112', 25591) as client:
-    basic_stats = client.stats() # Obtén estadísticas básicas
-    full_stats = client.stats(full=True) # Obtenga estadísticas completas
-
-print(basic_stats)
-print (full_stats)
